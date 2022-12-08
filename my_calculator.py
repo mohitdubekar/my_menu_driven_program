be3df964223b1bd030f7fc16fa614d24d52d3dc2
Mohit <mohitdubekar@gmail.com>
2022-12-08 15:25:02 +0530

    my_calculator_updated

--- my_calculator.py
+++ my_calculator.py
@@ -18,45 +18,45 @@ def divide(a, b):
     print(a, "/", b, "=", division)  
   
 # printing the heading  
-print("WELCOME TO A SIMPLE CALCULATOR")  
+#print("WELCOME TO A SIMPLE CALCULATOR")  
   
 # using the while loop to print menu list  
-while True:  
-    print("\nMENU")  
-    print("1. Sum of two Numbers")  
-    print("2. Difference between two Numbers")  
-    print("3. Product of two Numbers")  
-    print("4. Division of two Numbers")  
-    print("5. Exit")  
-    choice = int(input("\nEnter the Choice: "))  
+#while True:  
+    #print("\nMENU")  
+    #print("1. Sum of two Numbers")  
+    #print("2. Difference between two Numbers")  
+    #print("3. Product of two Numbers")  
+    #print("4. Division of two Numbers")  
+    #print("5. Exit")  
+    #choice = int(input("\nEnter the Choice: "))  
   
 # using if-elif-else statement to pick different options  
-    if choice == 1:  
-        print( "\nADDITION\n")  
-        a = int( input("First Number: "))  
-        b = int( input("Second Number: "))  
-        add(a, b)  
+    #if choice == 1:  
+    print( "\nADDITION\n")  
+    a = int( input("First Number: "))  
+    b = int( input("Second Number: "))  
+    add(5,5)  
   
-    elif choice == 2:  
-        print( "\nSUBTRACTION\n")  
-        a = int( input("First Number: "))  
-        b = int( input("Second Number: "))  
-        subtract(a, b)  
+    #elif choice == 2:  
+    print( "\nSUBTRACTION\n")  
+    a = int( input("First Number: "))  
+    b = int( input("Second Number: "))  
+    subtract(5,5)  
   
-    elif choice == 3:  
-        print( "\nMULTIPLICATION\n")  
-        a = int( input("First Number: "))  
-        b = int( input("Second Number: "))  
-        multiply(a, b)  
+    #elif choice == 3:  
+    print( "\nMULTIPLICATION\n")  
+    a = int( input("First Number: "))  
+    b = int( input("Second Number: "))  
+    multiply(5, 5)  
   
-    elif choice == 4:  
-        print( "\nDIVISION\n")  
-        a = int( input("First Number: "))  
-        b = int( input("Second Number: "))  
-        divide(a, b)  
+    #elif choice == 4:  
+    print( "\nDIVISION\n")  
+    a = int( input("First Number: "))  
+    b = int( input("Second Number: "))  
+    divide(5, 5)  
   
-    elif choice == 5:  
-        break  
+    #elif choice == 5:  
+        #break  
       
-    else:  
-        print( "Please Provide a valid Input!") 
+    #else:  
+        #print( "Please Provide a valid Input!") 
